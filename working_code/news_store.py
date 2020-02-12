c433e7b5ca6bdbdc28dd424d52a4a600f5860383
James Rowbottom <jr1419@cloud-vm-42-66.doc.ic.ac.uk>
2020-02-12 15:56:29 +0000

    deletes from It2 for master merge

--- working_code/news_store.py
+++ working_code/news_store.py
@@ -8,19 +8,17 @@ formatted_today='20' + today.strftime('%y%m%d')
 # print(formatted_today)
 today_news = news_today_dic()
 
-back_up_file_name = "news_in_" + formatted_today + ".json"
+back_up_file_name = '/vol/project/2019/545/g1954505/news-aggregation-system/news_archive/Backup' + "news_in_" + formatted_today + ".json"
 with open(back_up_file_name, 'w') as f:
     json.dump(today_news,f)
 print("successfully write for " + back_up_file_name)
 
-
-
-with open('news_history.json', 'r') as f:
+with open('/vol/project/2019/545/g1954505/news-aggregation-system/news_archive/news_history.json', 'r') as f:
     history_data = json.load(f)
 
 history_data[formatted_today] = today_news
 
-with open('news_history.json', 'w') as f:
+with open('/vol/project/2019/545/g1954505/news-aggregation-system/news_archive/news_history.json', 'w') as f:
     json.dump(history_data,f)
 
 print("finish updating")
