81828df442675b6c6e7619dd1c282e7294f2b840
Alfred Tingey <alfredtingey@alfreds-macbook.home>
2020-03-26 13:34:22 +0000

    master branch updated so that the app looks cool. Will now make iteration 3 app look similar

--- news_agg_app/app/__init__.py
+++ news_agg_app/app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -16,6 +17,8 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
